0776502368a36f928796dee0ef3caef4f131fa9d
Ramzi Zhu <ramzi.zhu@gmail.com>
2024-02-02 01:14:48 +1100

    updated to fix xivapi lodestone endpoints failing. still need to do more work changing everything over. atm using standalone nodestone

--- src/index.js
+++ src/index.js
@@ -3,31 +3,45 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
  
 
+
+
 // todo be able to search -> make this a function
 //const charName = "Chili Dog"
 //const server = "sephirot"
 
+
+// this is broken? -> xivpai lodestone raw endpoints do not work anymore
+// consider using https://github.com/xivapi/xivapi-js
+// https://github.com/xivapi/nodestone?tab=readme-ov-file#readme
+
+// const character = await this.characterParser.parse({ params: { characterId: id } });
+// console.log(character);
+
+
+// atm using standalone parser nodestone -> simply run standalone with yarn run express:start in folder.
+// figure out how to cloud deploy (droplet?) (google cloud?)
+
 async function fetchIDfromSearch(charName, server) {
   console.log(charName);
-  const urlString = "https://xivapi.com/character/search?name=" + charName + "&server=" + server;
+  const urlString = "http://localhost:8080/character/search?name=" + charName + "&server=" + server;
   let response = await fetch(urlString, { mode: 'cors' });
   let data = await response.json();
-  return data.Results[0].ID;
+  return data.List[0].ID; 
 }
 
 async function fetchDataFromID(ID) {
   ID = await ID;
-  const urlString = "https://xivapi.com/character/" + ID;
+  const urlString = "http://localhost:8080/character/" + ID + "?data=CJ";
   let response = await fetch(urlString, { mode: 'cors' });
   let data = await response.json();
 
   console.log(urlString)
   // USE THIS TO GRAB CHARA DATA
-  return data.Character; 
+  return data; 
 }
 
 
-
+// For now this stays
 class CharBox extends React.Component {
   constructor(props) {
     super(props);
@@ -37,9 +51,9 @@ class CharBox extends React.Component {
       formName: "Chili Dog",
       server: "Sephirot",
       charName: "",
-      activeJob: {ID: 1, Name: ""},
+      activeJob: null,
       activeJobLevel: 0,
-      jobs: Array(31).fill({Level: 0, UnlockedState: {ID: 0, Name:null}}),
+      jobs: {Paladin: {Unlockstate: "", Level: 0}},
       portraitUrl: null,
     };
 
@@ -63,22 +77,28 @@ class CharBox extends React.Component {
 
   render() {
     // uses data and adds it to state (does this once only)
+    // change this 
     if (!this.state.portraitUrl) {
       fetchDataFromID(fetchIDfromSearch(this.state.formName, this.state.server)).then(data => this.setState({
-        activeJob: data.ActiveClassJob.UnlockedState,
-        activeJobLevel: data.ActiveClassJob.Level,
-        portraitUrl: data.Portrait, 
+        activeJob: data.Character.ActiveClassjob,
+        activeJobLevel: data.Character.Level,
+        portraitUrl: data.Character.Portrait, 
         jobs: data.ClassJobs,
         charName: data.Name
       }));
     }
 
+    console.log(this.state.activeJob)
+    console.log(this.state.portraitUrl)
+    console.log(this.state.jobs);
     return (
       <div className='display'>
         <div className = 'portrait'>
           <div className = 'portraitInner'>
             <h1>{this.state.charName}</h1>
-            <p> {this.state.activeJob.Name} {this.state.activeJobLevel}</p>
+            <p>{this.state.jobs.Paladin.Unlockstate} {this.state.jobs.Paladin.Level}</p>
+            <img src={this.state.activeJob} alt="Active Job" width={"100"}/>
+            <p>{this.state.activeJobLevel}</p> 
             <div className = 'char'>
               <img src={this.state.portraitUrl} alt="Character Portrait" width={"350"}/>
             </div>
@@ -104,58 +124,58 @@ class CharBox extends React.Component {
             </form>
           </div>
         </div>
-        <div className = 'jobList'>
+        {/* <div className = 'jobList'>
           <div className = 'tanksDps'>
             <div className = 'tanks'>
               <h3>Tanks</h3>
-              <p>{this.state.jobs[0].UnlockedState.Name} {this.state.jobs[0].Level}</p>
-              <p>{this.state.jobs[1].UnlockedState.Name} {this.state.jobs[1].Level}</p>
-              <p>{this.state.jobs[2].UnlockedState.Name} {this.state.jobs[2].Level}</p>
-              <p>{this.state.jobs[3].UnlockedState.Name} {this.state.jobs[3].Level}</p>
+              <p>{this.state.jobs.Paladin.Unlockstate} {this.state.jobs.Paladin.Level}</p>
+              <p>{this.state.jobs[3].UnlockState} {this.state.jobs[3].Level}</p>
+              <p>{this.state.jobs[3].UnlockState} {this.state.jobs[3].Level}</p>
+              <p>{this.state.jobs[3].UnlockState} {this.state.jobs[3].Level}</p>
             </div>
             <div className = 'dps'>
             <h3>DPS</h3>
-              <p>{this.state.jobs[8].UnlockedState.Name} {this.state.jobs[8].Level}</p>
-              <p>{this.state.jobs[9].UnlockedState.Name} {this.state.jobs[9].Level}</p>
-              <p>{this.state.jobs[10].UnlockedState.Name} {this.state.jobs[10].Level}</p>
-              <p>{this.state.jobs[11].UnlockedState.Name} {this.state.jobs[11].Level}</p>
-              <p>{this.state.jobs[12].UnlockedState.Name} {this.state.jobs[12].Level}</p>
-              <p>{this.state.jobs[13].UnlockedState.Name} {this.state.jobs[13].Level}</p>
-              <p>{this.state.jobs[14].UnlockedState.Name} {this.state.jobs[14].Level}</p>
-              <p>{this.state.jobs[15].UnlockedState.Name} {this.state.jobs[15].Level}</p>
-              <p>{this.state.jobs[16].UnlockedState.Name} {this.state.jobs[16].Level}</p>
-              <p>{this.state.jobs[17].UnlockedState.Name} {this.state.jobs[17].Level}</p>
-              <p>{this.state.jobs[18].UnlockedState.Name} {this.state.jobs[18].Level}</p>
-              <p>{this.state.jobs[19].UnlockedState.Name} {this.state.jobs[19].Level}</p>
+              <p>{this.state.jobs[8].UnlockState} {this.state.jobs[8].Level}</p>
+              <p>{this.state.jobs[9].UnlockState} {this.state.jobs[9].Level}</p>
+              <p>{this.state.jobs[10].UnlockState} {this.state.jobs[10].Level}</p>
+              <p>{this.state.jobs[11].UnlockState} {this.state.jobs[11].Level}</p>
+              <p>{this.state.jobs[12].UnlockState} {this.state.jobs[12].Level}</p>
+              <p>{this.state.jobs[13].UnlockState} {this.state.jobs[13].Level}</p>
+              <p>{this.state.jobs[14].UnlockState} {this.state.jobs[14].Level}</p>
+              <p>{this.state.jobs[15].UnlockState} {this.state.jobs[15].Level}</p>
+              <p>{this.state.jobs[16].UnlockState} {this.state.jobs[16].Level}</p>
+              <p>{this.state.jobs[17].UnlockState} {this.state.jobs[17].Level}</p>
+              <p>{this.state.jobs[18].UnlockState} {this.state.jobs[18].Level}</p>
+              <p>{this.state.jobs[19].UnlockState} {this.state.jobs[19].Level}</p>
             </div>
           </div>
           <div className = 'healersOther'>
             <div className = 'healers'>
               <h3>Healers</h3>
-              <p>{this.state.jobs[4].UnlockedState.Name} {this.state.jobs[4].Level}</p>
-              <p>{this.state.jobs[5].UnlockedState.Name} {this.state.jobs[5].Level}</p>
-              <p>{this.state.jobs[6].UnlockedState.Name} {this.state.jobs[6].Level}</p>
-              <p>{this.state.jobs[7].UnlockedState.Name} {this.state.jobs[7].Level}</p>
+              <p>{this.state.jobs[4].UnlockState} {this.state.jobs[4].Level}</p>
+              <p>{this.state.jobs[5].UnlockState} {this.state.jobs[5].Level}</p>
+              <p>{this.state.jobs[6].UnlockState} {this.state.jobs[6].Level}</p>
+              <p>{this.state.jobs[7].UnlockState} {this.state.jobs[7].Level}</p>
             </div>
             <div className = 'DoH'>
             <h3>DoH</h3>
-              <p>{this.state.jobs[20].UnlockedState.Name} {this.state.jobs[20].Level}</p>
-              <p>{this.state.jobs[21].UnlockedState.Name} {this.state.jobs[21].Level}</p>
-              <p>{this.state.jobs[22].UnlockedState.Name} {this.state.jobs[22].Level}</p>
-              <p>{this.state.jobs[23].UnlockedState.Name} {this.state.jobs[23].Level}</p>
-              <p>{this.state.jobs[24].UnlockedState.Name} {this.state.jobs[24].Level}</p>
-              <p>{this.state.jobs[25].UnlockedState.Name} {this.state.jobs[25].Level}</p>
-              <p>{this.state.jobs[26].UnlockedState.Name} {this.state.jobs[26].Level}</p>
-              <p>{this.state.jobs[27].UnlockedState.Name} {this.state.jobs[27].Level}</p>
+              <p>{this.state.jobs[20].UnlockState} {this.state.jobs[20].Level}</p>
+              <p>{this.state.jobs[21].UnlockState} {this.state.jobs[21].Level}</p>
+              <p>{this.state.jobs[22].UnlockState} {this.state.jobs[22].Level}</p>
+              <p>{this.state.jobs[23].UnlockState} {this.state.jobs[23].Level}</p>
+              <p>{this.state.jobs[24].UnlockState} {this.state.jobs[24].Level}</p>
+              <p>{this.state.jobs[25].UnlockState} {this.state.jobs[25].Level}</p>
+              <p>{this.state.jobs[26].UnlockState} {this.state.jobs[26].Level}</p>
+              <p>{this.state.jobs[27].UnlockState} {this.state.jobs[27].Level}</p>
             </div>
             <div className = 'DoL'>
             <h3>DoL</h3>
-              <p>{this.state.jobs[28].UnlockedState.Name} {this.state.jobs[28].Level}</p>
-              <p>{this.state.jobs[29].UnlockedState.Name} {this.state.jobs[29].Level}</p>
-              <p>{this.state.jobs[30].UnlockedState.Name} {this.state.jobs[30].Level}</p>
+              <p>{this.state.jobs[28].UnlockState} {this.state.jobs[28].Level}</p>
+              <p>{this.state.jobs[29].UnlockState} {this.state.jobs[29].Level}</p>
+              <p>{this.state.jobs[30].UnlockState} {this.state.jobs[30].Level}</p>
             </div>
           </div>       
-        </div>
+        </div> */}
         <div className = 'footer'>
           <a href = "https://github.com/draco2169/chilidog.xiv" title = "Github Link" target = "_blank">@github.com/draco2169/chilidog.xiv</a>
         </div>
